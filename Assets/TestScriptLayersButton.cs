769727e4509ec7cb879d781d898565c0a2ecffe3
Konrad Öhman <konrad_o_92@hotmail.com>
2016-05-02 13:41:12 +0200

    Added stuff to main scene`

--- Assets/TestScriptLayersButton.cs
+++ Assets/TestScriptLayersButton.cs
@@ -3,10 +3,10 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class TestScriptLayersButton : MonoBehaviour {
-    
+    public Color activeColor;
+    public Color inactiveColor;
 	// Use this for initialization
 	void Start () {
-    
 	}
 	
 	// Update is called once per frame
@@ -16,12 +16,12 @@ public class TestScriptLayersButton : MonoBehaviour {
 
     public void MakeInactive()
     {
-        GetComponent<Button>().image.color = Color.grey;
+        GetComponent<Button>().image.color = inactiveColor;
 
     }
 
     public void MakeActive()
     {
-        GetComponent<Button>().image.color = Color.white;
+        GetComponent<Button>().image.color = activeColor;
     }
 }
