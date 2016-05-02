6ed95ff67bde98544c2a1c922027d5fc5c0f8c63
User <User>
2016-05-02 11:57:01 +0200

    Added view layer stuff

---
+++ Assets/TestScriptLayersButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TestScriptLayersButton : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void MakeInactive()
+    {
+        GetComponent<Button>().image.color = Color.grey;
+
+    }
+
+    public void MakeActive()
+    {
+        GetComponent<Button>().image.color = Color.white;
+    }
+}
