858aa2f3843865c2bf46e97a5f8163595dd984e7
Konrad Öhman <konrad_o_92@hotmail.com>
2016-05-02 12:14:02 +0200

    Putting stuff together

---
+++ Assets/SetPosScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetPosScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void SetMousePos()
+    {
+        transform.position=(Input.mousePosition);
+    }
+}
