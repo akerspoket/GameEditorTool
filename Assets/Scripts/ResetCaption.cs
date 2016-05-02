efd794887a63f72738afc968b88f5781de304639
Konrad Öhman <konrad_o_92@hotmail.com>
2016-05-02 11:23:36 +0200

    Top bar with options fixed`

---
+++ Assets/Scripts/ResetCaption.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ResetCaption : MonoBehaviour {
+    private Text captionText;
+    public string originalText;
+	// Use this for initialization
+	void Start () {
+        captionText = GetComponentInChildren<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        captionText.text = originalText;
+	}
+}
