b1f1b0316dc17bd73a78202a922e9e5058071de0
User <User>
2016-05-04 14:29:52 +0200

    Now uses distance to calculate charge (UNTESTED!)

--- Assets/UpdatePotentialFields.cs
+++ Assets/UpdatePotentialFields.cs
@@ -3,12 +3,17 @@ using System.Collections;
 
 public class UpdatePotentialFields : MonoBehaviour {
 
+    float distanceFactor = 1 / 40;
+
 	// Use this for initialization
 	void Start () {
+        Vector3 thisPosition = transform.position;
         GameObject[] ground = GameObject.FindGameObjectsWithTag("Respawn");
         foreach (GameObject cube in ground)
         {
-            GetComponent<PotentialFieldCharge>().AddCharge(0.1f);
+            Vector3 cubePosition = cube.transform.position;
+            float distance = (cubePosition - thisPosition).magnitude;
+            GetComponent<PotentialFieldCharge>().AddCharge(distance * distanceFactor);
         }
 	}
 	
