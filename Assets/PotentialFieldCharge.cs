a3ae7681e76200af34fa3b3e3649660e70e6f986
User <User>
2016-05-04 15:01:26 +0200

    Prefabs exist for sources

--- Assets/PotentialFieldCharge.cs
+++ Assets/PotentialFieldCharge.cs
@@ -21,11 +21,15 @@ public class PotentialFieldCharge : MonoBehaviour
 
     public void AddCharge(float p_charge)
     {
+        // Accumulate total charge
         charge += p_charge;
+
+        float colorFactor = charge / 40.0f; // Silly hard-coded value
+        // Color the cube properly
         if (charge > 0)
-            GetComponent<Renderer>().material.color = new Color(0.5f + charge, 0.5f - charge, 0.5f - charge, 1);
+            GetComponent<Renderer>().material.color = new Color(0.5f + colorFactor, 0.5f - colorFactor, 0.5f - colorFactor, 1);
         else if (charge < 0)
-            GetComponent<Renderer>().material.color = new Color(0.5f + charge, 0.5f - charge, 0.5f + charge, 1);
+            GetComponent<Renderer>().material.color = new Color(0.5f + colorFactor, 0.5f - colorFactor, 0.5f + colorFactor, 1);
     }
 
 }
--- Assets/UpdatePotentialFields.cs
+++ Assets/UpdatePotentialFields.cs
@@ -3,19 +3,22 @@ using System.Collections;
 
 public class UpdatePotentialFields : MonoBehaviour {
 
-    float distanceFactor = 1 / 40;
+    //float distanceFactor = 1.0f / 20.0f;
+    public float chargeValue = 0;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         Vector3 thisPosition = transform.position;
         GameObject[] ground = GameObject.FindGameObjectsWithTag("Respawn");
         foreach (GameObject cube in ground)
         {
             Vector3 cubePosition = cube.transform.position;
             float distance = (cubePosition - thisPosition).magnitude;
-            GetComponent<PotentialFieldCharge>().AddCharge(distance * distanceFactor);
+
+            float charge = chargeValue / distance;
+            cube.GetComponent<PotentialFieldCharge>().AddCharge(charge);
         }
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
