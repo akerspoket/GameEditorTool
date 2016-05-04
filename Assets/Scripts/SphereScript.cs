aa2bf20a79c67cd8252d13b8358c47a3b947c07a
Stureplan <linus.ax@gmail.com>
2016-05-04 15:19:52 +0200

    stuff for azozoka

--- Assets/Scripts/SphereScript.cs
+++ Assets/Scripts/SphereScript.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class SphereScript : MonoBehaviour {
     private bool Creating = false;
-    GameObject sphere;
+    GameObject m_object;
     // Use this for initialization
     void Start () {
 	
@@ -17,16 +17,16 @@ public class SphereScript : MonoBehaviour {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit))
             {
-                if (hit.transform != sphere.transform)
+                if (hit.transform != m_object.transform)
                 {
                     Vector3 derp = hit.transform.position - Camera.main.transform.position;
                     Vector3 newPos = hit.transform.position - derp.normalized;
-                    sphere.transform.position = newPos;
+                    m_object.transform.position = newPos;
                 }
             }
             else
             {
-                sphere.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x
+                m_object.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x
                     , Input.mousePosition.y, 10));
             }
             if (Input.GetMouseButtonDown(0))
@@ -36,10 +36,10 @@ public class SphereScript : MonoBehaviour {
         }
 	}
 
-    public void CreateSphere()
+    public void CreateObject(GameObject objectToCreate)
     {
-        sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        sphere.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x
+        m_object = Instantiate(objectToCreate);
+        m_object.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x
                 , Input.mousePosition.y, 10));
         Creating = true;
     }
