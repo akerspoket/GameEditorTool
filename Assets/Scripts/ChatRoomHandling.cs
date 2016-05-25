614f06ab507f586dc8b36f0b147500524f13b669
Konrad Öhman <konrad_o_92@hotmail.com>
2016-05-25 14:45:09 +0200

    Chat box now handles the input!

---
+++ Assets/Scripts/ChatRoomHandling.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ChatRoomHandling : MonoBehaviour {
+    public GameObject ChatRoomPrefab;
+    private List<GameObject> rooms = new List<GameObject>();
+    private int CurrentRoom = 0;
+    private InputField inputField;
+    private string standardText;
+	// Use this for initialization
+	void Start () {
+        int numberOfRooms = GetComponentInChildren<Dropdown>().options.Count;
+        GameObject newRoom;
+        for (int i = 0; i < numberOfRooms; i++)
+        {
+            newRoom = Instantiate(ChatRoomPrefab);
+            newRoom.transform.SetParent(this.transform);
+            newRoom.transform.localPosition = ChatRoomPrefab.transform.localPosition;
+            newRoom.transform.localScale = new Vector3(1, 1, 1);
+            newRoom.GetComponent<RectTransform>().sizeDelta = ChatRoomPrefab.GetComponent<RectTransform>().sizeDelta;
+            newRoom.SetActive(false);
+            rooms.Add(newRoom);
+        }
+        SetCurrentChatRoom(0);
+        GetComponentInChildren<Dropdown>().onValueChanged.AddListener(SetCurrentChatRoom);
+        inputField = GetComponentInChildren<InputField>();
+        standardText = inputField.text;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SetCurrentChatRoom(int roomNumber)
+    {
+        rooms[CurrentRoom].SetActive(false);
+        rooms[roomNumber].SetActive(true);
+        CurrentRoom = roomNumber;
+    }
+
+    public void SendComment()
+    {
+        string text = inputField.text;
+        inputField.text = "";
+
+        text = "You: " + text;
+        rooms[CurrentRoom].GetComponent<ChatRoomTextHandling>().PrintText(text);
+    }
+
+    public void GetComment()
+    {
+
+    }
+}
---
+++ Assets/Scripts/ChatRoomTextHandling.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ChatRoomTextHandling : MonoBehaviour {
+
+    public GameObject ChatTextPrefab;
+    private List<GameObject> messages = new List<GameObject>();
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void PrintText(string text)
+    {
+        float offsetY = 0;
+        for (int i = 0; i < messages.Count; i++)
+        {
+            offsetY += messages[i].GetComponent<Text>().rectTransform.sizeDelta.y;
+        }
+
+        GameObject newChatText = Instantiate(ChatTextPrefab);
+        newChatText.transform.SetParent(this.transform);
+        //newChatText.transform.localPosition = ChatTextPrefab.transform.localPosition;
+        newChatText.transform.localScale = new Vector3(1, 1, 1);
+        newChatText.GetComponent<RectTransform>().sizeDelta = ChatTextPrefab.GetComponent<RectTransform>().sizeDelta;
+        newChatText.GetComponent<Text>().text = text;
+
+        newChatText.GetComponent<RectTransform>().localPosition = new Vector3(-140, -offsetY + 100, 0); // This is hardcoded cuz it didnt want to be at 0,0,0
+        messages.Add(newChatText);
+    }
+}
