b39688042240464a872a003eb7acfb971d203ac1
Jawsarn <christian_markowicz@hotmail.com>
2016-05-25 22:26:03 +0200

    Added hover tool

---
+++ Assets/ShowOnHover.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowOnHover : MonoBehaviour {
+
+    public GameObject[] hoverObjects;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var rect = GetComponent<RectTransform>().rect; //new Rect(0, 0, 150, 150);
+        rect.position += new Vector2(transform.position.x, transform.position.y);
+        if (rect.Contains(Input.mousePosition))
+        {
+            foreach (var item in hoverObjects)
+            {
+                item.SetActive(true);
+            }
+        }
+        else
+        {
+            foreach (var item in hoverObjects)
+            {
+                item.SetActive(false);
+            }
+        }       
+	}
+}
